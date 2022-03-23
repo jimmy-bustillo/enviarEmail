a2b37e93273d312f82ae35b61d98f1625965464f
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-03-23 12:46:56 -0600

    agrega función para validar si el campo esta vacio

--- js/app.js
+++ js/app.js
@@ -1,14 +1,56 @@
 // Variables
 const enviarBtn = document.querySelector("#enviar");
+const formulario = document.querySelector("#enviar-mail");
+
+// Variables para campos
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
 
 // EventListeners
 eventListeners();
 function eventListeners() {
+  // Cuando la app arranca
   document.addEventListener("DOMContentLoaded", iniciarApp);
+
+  // Campos del formulario
+  email.addEventListener("blur", validarFormulario);
+  asunto.addEventListener("blur", validarFormulario);
+  mensaje.addEventListener("blur", validarFormulario);
 }
 
 // Funciones
 function iniciarApp() {
   enviarBtn.disabled = true;
   enviarBtn.classList.add("cursor-not-allowed", "opacity-50");
+}
+
+// Valida el formulario
+function validarFormulario(e) {
+  if (e.target.value.length > 0) {
+    e.target.classList.remove("border", "border-red-500");
+  } else {
+    e.target.classList.add("border", "border-red-500");
+
+    mostrarError();
+  }
+}
+
+function mostrarError() {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = "Todos los campos son obligatorios";
+  mensajeError.classList.add(
+    "border",
+    "border-red-500",
+    "background-red-100",
+    "text-red-500",
+    "p-3",
+    "mt-5",
+    "text-center",
+    "error"
+  );
+  const errores = document.querySelectorAll(".error");
+  if (errores.length === 0) {
+    formulario.appendChild(mensajeError);
+  }
 }
